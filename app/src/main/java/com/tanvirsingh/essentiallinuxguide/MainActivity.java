36f62b2ce0be42ef56d3b82e295adcf6087f1cfc
Tanvir Singh <tanvirs137@gmail.com>
2016-10-11 15:44:13 +0530

    setup toolbar and tabs

---
+++ app/src/main/java/com/tanvirsingh/essentiallinuxguide/MainActivity.java
@@ -0,0 +1,23 @@
+package com.tanvirsingh.essentiallinuxguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.support.design.widget.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Adding Toolbar to Main screen
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Tab 1"));
+        tabs.addTab(tabs.newTab().setText("Tab 2"));
+        tabs.addTab(tabs.newTab().setText("Tab 3"));
+    }
+}
