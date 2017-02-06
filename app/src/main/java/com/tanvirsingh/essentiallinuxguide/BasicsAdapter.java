8dd905ff4fe5ee02a2ca90798c24c4c1d471b816
tnvrsingh <tanvirs137@gmail.com>
2017-02-07 01:50:37 +0530

    Recycler view onClickListener activated on BasicsList

--- app/src/main/java/com/tanvirsingh/essentiallinuxguide/BasicsAdapter.java
+++ app/src/main/java/com/tanvirsingh/essentiallinuxguide/BasicsAdapter.java
@@ -1,12 +1,21 @@
 package com.tanvirsingh.essentiallinuxguide;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
+
+import static android.content.ContentValues.TAG;
+import static java.security.AccessController.getContext;
+
+
 
 /**
  * Created by Tanvir on 12/24/2016.
@@ -15,12 +24,36 @@ public class BasicsAdapter extends RecyclerView.Adapter<BasicsAdapter.MyViewHold
 
     private List<Basic> basicsList;
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder{
         public TextView title;
 
         public MyViewHolder(View view){
             super(view);
             title = (TextView)view.findViewById(R.id.title);
+
+            view.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "Element " + getAdapterPosition() + " clicked.");
+
+                    Context context = itemView.getContext();
+
+                    Intent intent;
+
+                    switch(getAdapterPosition()){
+                        case 0:
+                            intent =  new Intent(context, MainActivity.class);
+                            break;
+                        case 1:
+                            intent =  new Intent(context, MainActivity.class);
+                            break;
+                        default:
+                            intent =  new Intent(context, MainActivity.class);
+                            break;
+                    }
+                    context.startActivity(intent);
+                }
+            });
         }
     }
 
